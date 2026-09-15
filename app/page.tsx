3206e316b4fb5058074b5e4dfa49326dcd3bf607
kame432 <youyiguijing962@gmail.com>
2026-09-15 15:26:40 +0900

    feat: ロゴ追加

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
+          className="dark:invert w-[100px]"
+          src="/logo.png"
+          alt="lisearch logo"
           width={100}
-          height={20}
+          height={100}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
