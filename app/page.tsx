4d9eaae04aa6841b168b5860bce77e9ce6b54d66
manyoukazu <shishanwanye@gmail.com>
2026-09-15 15:04:09 +0900

    簡単な説明分の追加

--- app/page.tsx
+++ app/page.tsx
@@ -17,21 +17,7 @@ export default function Home() {
             lisearch
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+            英語研究発表を​聞き取れるようになりたい​日本人学生向けの、​リスニングアプリです。​
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
